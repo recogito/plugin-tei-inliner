9faf06035d00796681c5face17f1f3327c056560
Rainer Simon <rainer@rainersimon.io>
2025-05-19 12:42:04 +0200

    Cleanup

--- src/extensions/ExportInlinedTEIMenuItem.tsx
+++ src/extensions/ExportInlinedTEIMenuItem.tsx
@@ -1,15 +1,12 @@
 import { Code } from '@phosphor-icons/react';
-import * as Dropdown from '@radix-ui/react-dropdown-menu';
 import { DocumentCardActionsExtensionProps } from '@recogito/studio-sdk';
 
 export const ExportInlinedTEIMenuItem = (props: DocumentCardActionsExtensionProps) => {
 
   return (
-    <Dropdown.Item className="dropdown-item">
-      <a href={`/api/${props.projectId}/${props.document?.id}/export/inline-tei`}>
-        <Code size={16}/> Export Flattened TEI 
-      </a>
-    </Dropdown.Item>
+    <a href={`/api/${props.projectId}/${props.document?.id}/export/inline-tei`}>
+      <Code size={16}/> Export Flattened TEI 
+    </a>
   )
 
 }
